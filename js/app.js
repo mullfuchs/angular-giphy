9a3c3309dace17127cffc2ac3fe1666930488a12
mullfuchs <burke.leo@gmail.com>
2016-09-16 13:15:34 -0700

    work

--- js/app.js
+++ js/app.js
@@ -1,5 +1,25 @@
 angular.module('GiphyApp', [])
 
-.run(function() {
-  console.log('App has loaded!');
-});
+// .run(function() {
+//   console.log('App has loaded!');
+// });
+
+.controller('GiphyCtrl', ['$scope', '$http', function($scope, $http){
+  $scope.searchTerm = "foxes"
+
+  $scope.gifs = [];
+
+  $scope.getGifs = function(){
+    var request = {
+      url: 'http://api.giphy.com/v1/gifs/search?q=' + $scope.searchTerm + '&api_key=dc6zaTOxFJmzC',
+      method: 'GET'
+    }
+
+    $http(request).then(function success(res){
+      $scope.gifs = res.data.data;
+      console.log($scope.gifs);
+    }, function error(res){
+      console.log(res);
+    });
+  }
+}]);
